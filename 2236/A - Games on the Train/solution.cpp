c65f460cd5a109aa33e887213d359dc0dc83f491
Mayankkvv <145976204+Mayankkvv@users.noreply.github.com>
2026-06-29 19:03:10 +0530

    Add Games on the Train [A] from Codeforces

---
+++ 2236/A - Games on the Train/solution.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int mn = INT_MAX, mx = INT_MIN;
+        for (int i = 0; i < n; i++) {
+            int h;
+            cin >> h;
+            mn = min(mn, h);
+            mx = max(mx, h);
+        }
+        cout << (mx - mn + 1) << '
+';
+    }
+    return 0;
+}
