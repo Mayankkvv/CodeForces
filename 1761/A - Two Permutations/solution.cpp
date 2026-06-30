238f6454db4e889a72ad76a6341da95b53ab4f41
Mayankkvv <145976204+Mayankkvv@users.noreply.github.com>
2026-06-30 21:34:47 +0530

    Add Two Permutations [A] from Codeforces

---
+++ 1761/A - Two Permutations/solution.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int n, a, b;
+        cin >> n >> a >> b;
+        if(a+b + 2 <= n || (a == b && a == n )){
+          cout<< "YES
+";
+        }else{
+          cout<<"NO
+";
+        }
+    }
+ 
+    return 0;
+}
