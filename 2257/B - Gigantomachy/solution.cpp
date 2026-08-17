1558749232c10efd22bf39bd13b916efd78f4ca9
Mayankkvv <145976204+Mayankkvv@users.noreply.github.com>
2026-08-17 20:26:48 +0530

    Add Gigantomachy [B] from Codeforces

---
+++ 2257/B - Gigantomachy/solution.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+ 
+class DisjointSet {
+    vector<int> rank, parent, size;
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+ 
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+ 
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+ 
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+ 
+        long long a1 = 0, b1 = 0;
+        for (int i = 0; i < n; i++) {
+            long long x;
+            cin >> x;
+            if (i == 0) a1 = x;
+        }
+        for (int i = 0; i < m; i++) {
+            long long x;
+            cin >> x;
+            if (i == 0) b1 = x;
+        }
+        long long TA = a1 + n; 
+        long long TB = b1 + m;
+        cout << (TA >= TB ? 1 : 2) << "
+";
+    }
+ 
+    return 0;
+}
